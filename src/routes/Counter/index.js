8081a290bd792e72b97190c4d74d2e72dcc15d39
Hung Le <hung.le@bluecomgroup.com>
2016-07-11 19:33:44 +0700

    update changed at State - https://suspicious.website/2016/04/29/starting-out-with-react-redux-starter-kit/

--- src/routes/Counter/index.js
+++ src/routes/Counter/index.js
@@ -7,8 +7,7 @@ export default (store) => ({
     /*  Webpack - use 'require.ensure' to create a split point
         and embed an async module loader (jsonp) when bundling   */
     require.ensure([], (require) => {
-      /*  Webpack - use require callback to define
-          dependencies for bundling   */
+      /*  Webpack - use require callback to define dependencies for bundling   */
       const Counter = require('./containers/CounterContainer').default
       const reducer = require('./modules/counter').default
 
